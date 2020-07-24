0695df36864d935ccbde253e7c9d770b20e6e750
Jessica <duellje@oregonstate.edu>
2020-07-24 11:20:11 -0400

    gets token from api and then sets token to localstorage and navigated to bubblePage

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,7 @@
 import React, {useState} from "react";
+import axios from 'axios';
 
 const Login = props => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
   const {history} = props;
 
   const initialLoginValues = {
@@ -13,11 +12,22 @@ const Login = props => {
   const [loginFormValues, setValues] = useState(initialLoginValues);
 
   const handleChanges = e => {
-
+    setValues({...loginFormValues, [e.target.name]: e.target.value})
   }
 
   const login = e => {
     e.preventDefault();
+    axios 
+      .post('http://localhost:5000/api/login', loginFormValues)
+      .then(res => {
+          //set token to localstorage
+          localStorage.setItem('token', res.data.payload);
+          history.push('/bubblePage');
+      })
+      .catch(err => console.log(err))
+      .finally(
+        setValues(initialLoginValues)
+      )
   }
 
   return (
@@ -37,13 +47,14 @@ const Login = props => {
           <label htmlFor="password">
             Password: &nbsp;
             <input
-              type="text"
+              type="password"
               name="password"
               id="password"
               onChange={handleChanges}
               value={loginFormValues.password}
             />
           </label>
+          <button>Login</button>
       </form>
     </>
   );
